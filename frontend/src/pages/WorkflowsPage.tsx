23068502bb2ab641a0d4fe908e52c9c090f99645
ludovic <ludovic@scilink.net>
2026-02-11 12:08:05 +0100

    The Devices tab in the workflows sidebar now shows the list of installed devices from devicesStore

--- frontend/src/pages/WorkflowsPage.tsx
+++ frontend/src/pages/WorkflowsPage.tsx
@@ -3,6 +3,7 @@ import { Tabs } from "@mantine/core";
 import { IconPuzzle, IconCpu, IconSitemap } from "@tabler/icons-react";
 import { ReactFlowProvider } from "reactflow";
 import FlowCanvas from "../components/FlowCanvas";
+import SidebarDevicesList from "../components/SidebarDevicesList";
 import "./WorkflowsPage.css";
 
 const WorkflowsPage = () => {
@@ -28,7 +29,7 @@ const WorkflowsPage = () => {
                         <p className="sidebar-empty">Block pieces will appear here</p>
                     </Tabs.Panel>
                     <Tabs.Panel value="devices" className="sidebar-panel">
-                        <p className="sidebar-empty">Installed devices will appear here</p>
+                        <SidebarDevicesList />
                     </Tabs.Panel>
                     <Tabs.Panel value="workflows" className="sidebar-panel">
                         <p className="sidebar-empty">Sub-workflows will appear here</p>
