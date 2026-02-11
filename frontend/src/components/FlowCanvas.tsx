6cc26219764ef603b3979a92a38df290d5361011
ludovic <ludovic@scilink.net>
2026-02-11 11:40:26 +0100

    add ractflow canvas, setup workflow page

--- frontend/src/components/FlowCanvas.tsx
+++ frontend/src/components/FlowCanvas.tsx
@@ -1,33 +1,46 @@
-import { useNodesState, useEdgesState, addEdge } from 'reactflow';
+import ReactFlow, {
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    Controls,
+    Background,
+    BackgroundVariant,
+    type Edge,
+    type Node,
+    type OnConnect,
+} from "reactflow";
+import "reactflow/dist/style.css";
 
-const initialNodes = [
-  {
-    id: '1',
-    position: { x: 100, y: 100 },
-    data: { label: 'Hello React Flow' }
-  }
+const initialNodes: Node[] = [
+    {
+        id: "1",
+        position: { x: 100, y: 100 },
+        data: { label: "Hello React Flow" },
+    },
 ];
 
-const initialEdges = [];
+const initialEdges: Edge[] = [];
 
 function FlowCanvas() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const [nodes, , onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = (params) =>
-    setEdges((eds) => addEdge(params, eds));
+    const onConnect: OnConnect = (params) =>
+        setEdges((eds) => addEdge(params, eds));
 
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      />
-    </div>
-  );
+    return (
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            fitView
+        >
+            <Controls />
+            <Background variant={BackgroundVariant.Dots} gap={16} size={1} />
+        </ReactFlow>
+    );
 }
 
 export default FlowCanvas;
--- frontend/src/pages/WorkflowsPage.tsx
+++ frontend/src/pages/WorkflowsPage.tsx
@@ -1,15 +1,15 @@
-import { Title, Text, Container } from "@mantine/core";
-import { useTranslation } from "react-i18next";
+import { ReactFlowProvider } from "reactflow";
+import FlowCanvas from "../components/FlowCanvas";
+import "./WorkflowsPage.css";
 
 const WorkflowsPage = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Container>
-      <Title order={2}>{t("nav.workflows")}</Title>
-      <Text mt="md">Workflows page content goes here.</Text>
-    </Container>
-  );
+    return (
+        <div className="workflows-page">
+            <ReactFlowProvider>
+                <FlowCanvas />
+            </ReactFlowProvider>
+        </div>
+    );
 };
 
 export default WorkflowsPage;
